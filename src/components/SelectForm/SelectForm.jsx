eedd8ef58af213df7d36b22d00e69e5f6638a99b
Amruta Marathe <amruta.marathe25@gmail.com>
2023-09-13 21:00:43 +1000

    initial commit

--- src/components/SelectForm/SelectForm.jsx
+++ src/components/SelectForm/SelectForm.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-//import currencies from "../../supportedCurrencies.json";
 
-export const SelectForm = () => {
+ const SelectForm = () => {
   return (
     <>
       <div className={style.priceContainer}>Price container</div>
@@ -12,14 +11,11 @@ export const SelectForm = () => {
               {currency.currency}
             </option>
           ))}
-          <option value="1">Angular</option>
-          <option value="2">React</option>
-          <option value="3">Vue.js</option>
-          <option value="4">Ember.js</option>
+
         </select>
       </div>
     </>
   );
 };
 
-// export default SelectForm;
+ export default SelectForm;
